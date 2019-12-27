cb8ce217eb5ee86c2b307891606c108698518e79
Vladimir <Vovchik.balagula@gmail.com>
2019-12-27 15:53:19 +0200

    start to train part3
    Signed-off-by: Vladimir <Vovchik.balagula@gmail.com>

--- Part3/dataloader.py
+++ Part3/dataloader.py
@@ -56,28 +56,32 @@ class PyTorchDataset(torch.utils.data.Dataset):
         1. On-demand normalization
         2. Returns torch.tensor instead of ndarray
     """
+
+    word_to_num = None
+    target_to_num = None
+    num_to_target = None
+
     def __init__(self, path):
         X, y = PyTorchDataset.load_file(path)
 
-        vocab = np.unique(X)
-        unique_target = np.unique(y)
-
-        word_to_num = dict(zip(vocab, range(len(vocab))))
-        target_to_num = dict(zip(unique_target, range(len(unique_target))))
-
-        self.sequences, self.targets = PyTorchDataset.create_sentences(X, y, word_to_num, target_to_num)
+        if self.word_to_num is None:
+            vocab = np.unique(np.append(X, ['UUUNNNKKK']))
+            unique_target = np.unique(y)
+            PyTorchDataset.word_to_num = dict(zip(vocab, range(len(vocab))))
+            PyTorchDataset.target_to_num = dict(zip(unique_target, range(len(unique_target))))
+            PyTorchDataset.num_to_target = {k:v for v,k in self.target_to_num.items()}
+        self.sequences, self.targets = PyTorchDataset.create_sentences(X, y,
+                                                                       PyTorchDataset.word_to_num,
+                                                                       PyTorchDataset.target_to_num)
         # print(self.sequences[:2])
         # print(self.targets[:2])
-
 
     def __getitem__(self, idx):
         x, y = self.sequences[idx], self.targets[idx]
 
-
         data = torch.tensor(x, dtype=torch.long)
         target = torch.tensor(y)
         return (data, target)
-
 
     def __len__(self):
         return len(self.sequences)
@@ -93,35 +97,17 @@ class PyTorchDataset(torch.utils.data.Dataset):
         arr_per_sequences = np.split(data, ind)
         target_per_sequences = np.split(target, ind)
 
-        sentences = []
-        for seq in arr_per_sequences:
-            temp = []
-            for word in seq:
-                temp.append(word_to_num.get(word))
-            sentences.append(temp)
-
-        targets = []
-        for seq in target_per_sequences:
-            temp = []
-            for word in seq:
-                temp.append(target_to_num.get(word))
-            targets.append(temp)
-
-
-        # sentences = [' '.join(word_to_num.get(word)) for seq in arr_per_sequences for word in seq]
-        #
-        # target_per_sequences = np.split(target, ind)
-        # targets = [' '.join(target_to_num.get(tar)) for seq in target_per_sequences for tar in target]
+        sentences = [[word_to_num.get(word, word_to_num['UUUNNNKKK']) for word in seq] for seq in arr_per_sequences]
+        targets = [[target_to_num.get(word) for word in seq] for seq in target_per_sequences]
         return sentences, targets
+
 
 if __name__ == '__main__':
     # Must change
-    root = Path('DL_3/Part3/{}'.format(sys.argv[1]))
+    #root = Path('DL_3/Part3/{}'.format(sys.argv[1]))
+    train_file = sys.argv[1]
 
-    # Path to data files
-    train_file = root / "train"
-
-    dataset = PyTorchDataset('/home/vova/PycharmProjects/deep_exe3/DL_3/Part3/ner/train')
+    dataset = PyTorchDataset(train_file)
 
     dataloader_train = DataLoader(dataset, batch_size=50, shuffle=True, collate_fn=pad_collate)
 
