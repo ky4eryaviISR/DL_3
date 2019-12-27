29325cb46dd77647478257ff6e2db6876486bea5
VovaPsevkin <psevkin@gmail.com>
2019-12-27 16:32:25 +0200

    Char split

--- Part3/dataloader.py
+++ Part3/dataloader.py
@@ -4,45 +4,11 @@ from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from pathlib import Path
 import sys
-
-# class CustomerDataSet(Dataset):
-#     def __init__(self, path, encoded_words=None, encoded_labels=None):
-#         pre = Preprocessing(path)
-#
-#         if not encoded_words:
-#             vocab, labels, encoded_words, encoded_labels, encode_num_to_labels = pre.get_vocab_and_labels()
-#             tokens = pre.load_data()
-#             contexts = pre.get_contexts(tokens, encoded_words, encoded_labels)
-#
-#             self.sample, self.label = list(zip(*contexts))
-#             self.vocab = vocab
-#             self.labels = labels
-#             self.encoded_words = encoded_words
-#             self.encoded_labels= encoded_labels
-#             self.encode_num_to_label = encode_num_to_labels
-#         else:
-#             encoded_words = encoded_words
-#             encoded_labels = encoded_labels
-#             tokens = pre.load_data()
-#             contexts = pre.get_contexts(tokens, encoded_words, encoded_labels)
-#             self.sample, self.label = list(zip(*contexts))
-#
-#     def __getitem__(self, index):
-#
-#         label = torch.tensor(self.label[index], dtype=torch.long)
-#         sample = torch.tensor(self.sample[index])
-#         return sample, label
-#
-#     def __len__(self):
-#         return len(self.sample)
-
 import numpy as np
 
 
 def pad_collate(batch):
   (xx, yy) = zip(*batch)
-  # x_lens = [len(x) for x in xx]
-  # y_lens = [len(y) for y in yy]
 
   xx_pad = pad_sequence(xx, batch_first=True, padding_value=0)
   yy_pad = pad_sequence(yy, batch_first=True, padding_value=0)
@@ -73,8 +39,6 @@ class PyTorchDataset(torch.utils.data.Dataset):
         self.sequences, self.targets = PyTorchDataset.create_sentences(X, y,
                                                                        PyTorchDataset.word_to_num,
                                                                        PyTorchDataset.target_to_num)
-        # print(self.sequences[:2])
-        # print(self.targets[:2])
 
     def __getitem__(self, idx):
         x, y = self.sequences[idx], self.targets[idx]
@@ -102,12 +66,29 @@ class PyTorchDataset(torch.utils.data.Dataset):
         return sentences, targets
 
 
+class CharDataset(PyTorchDataset):
+
+    def __init__(self, path):
+        super().__init__(path)
+        X, y = PyTorchDataset.load_file(path)
+        print(X)
+        i = 0
+
+    @staticmethod
+    def preprocessing_char(X):
+        for word in X:
+            for char in word:
+                print(char)
+
+
+
+
 if __name__ == '__main__':
     # Must change
     #root = Path('DL_3/Part3/{}'.format(sys.argv[1]))
     train_file = sys.argv[1]
-
-    dataset = PyTorchDataset(train_file)
+    train_file = r'/home/vova/PycharmProjects/deep_exe3/DL_3/Part3/ner/train'
+    dataset = CharDataset(train_file)
 
     dataloader_train = DataLoader(dataset, batch_size=50, shuffle=True, collate_fn=pad_collate)
 
